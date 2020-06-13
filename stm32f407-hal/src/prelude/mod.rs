a80e2b25c10c2fc30eb777bce98c924d7e108a5b
Win <win@Wins-MacBook-Pro.local>
2020-06-13 22:50:46 +0800

    refactor prelude in hal

--- stm32f407-hal/src/prelude/mod.rs
+++ stm32f407-hal/src/prelude/mod.rs
@@ -1,12 +1,3 @@
-pub use embedded_hal::digital::v2::InputPin;
-pub use embedded_hal::digital::v2::OutputPin;
-pub use embedded_hal::digital::v2::StatefulOutputPin;
-pub use embedded_hal::digital::v2::ToggleableOutputPin;
-pub use embedded_hal::prelude::*;
+mod prelude;
 
-pub use crate::gpio::GpioExt as _stm32f4xx_hal_gpio_GpioExt;
-// pub use crate::i2c::Pins as _stm32f4xx_hal_i2c_Pins;
-pub use crate::rcc::RccExt as _stm32f4xx_hal_rcc_RccExt;
-
-
-pub use crate::time::U32Ext as _stm32f4xx_hal_time_U32Ext;
+pub use prelude::*;
